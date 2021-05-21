7eb0ecc6b506e56962ca4fcfe1b3c507bd778f6b
Jonathan Richards <jrichards050399@gmail.com>
2021-05-21 10:59:38 -0400

    Module 5 changes

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      //<> is shorthand for React.Fragment
+      <>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
